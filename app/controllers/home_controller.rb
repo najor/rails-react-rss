82d21861fba031403eaf1a84026a204ecd8d70ca
najorcruzcruz <najorcruzcruz@gmail.com>
2016-08-20 22:51:30 +0100

    Move Helper to lib folder.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,13 @@
 require 'rss'
-require 'home_helper'
+require 'rss_helper'
 
 class HomeController < ApplicationController
-  include HomeHelper
   URLS = %w(http://www.cnet.com/rss/news/ http://www.pcworld.com/index.rss)
 
   def index
     url = params[:url]
     url = URLS[0] if url.blank?
 
-    render component: 'Home', props: {articles: get_rss(url), url: url}
+    render component: 'Home', props: {articles: RSSHelper.get_rss(url), url: url}
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,21 +1,3 @@
 module HomeHelper
 
-  def get_rss (url)
-    open(url) do |rss|
-      feed = RSS::Parser.parse(rss)
-      articles = []
-      i = 0
-      feed.items.each do |item|
-        i += 1
-        articles.push({
-                        title: item.title,
-                        description: item.description,
-                        link: item.link,
-                        id: i
-                      })
-
-      end
-      return articles
-    end
-  end
 end
